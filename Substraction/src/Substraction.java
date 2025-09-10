f9fc6e1ba636f654f78fb9e59ec89d5deb0f5206
unknown <ruturajwavhal10@gmail.com>
2025-09-10 12:09:38 +0530

    sub file added

---
+++ Substraction/src/Substraction.java
@@ -0,0 +1,11 @@
+public class Substraction {
+    public static void main(String args[])
+    {
+        /**
+        Substraction of two numbers 
+         */
+        int a = 40;
+        int b = 30;
+        System.out.println("Substraction of " + a + " and " + b + " is : " + (a - b));
+    }
+}
