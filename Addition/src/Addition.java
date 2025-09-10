dc2f42d09d408bb85027e9e05762637cbc326aa9
unknown <ruturajwavhal10@gmail.com>
2025-09-10 11:54:25 +0530

    addition project is added

---
+++ Addition/src/Addition.java
@@ -0,0 +1,11 @@
+public class Addition {
+    public static void main(String [] args)
+    {
+        /**
+         * Here we're initializing two variables and printing its addition
+         */
+        int a = 10;
+        int b = 20;
+        System.out.println("Addition of " + a + " and " + b + " is " + (a + b));
+    }
+}
